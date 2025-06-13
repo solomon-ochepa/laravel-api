c8cb611f8d24faffaf894908847ec56a154ca2fc
elisha-mustapha <elisha-mustapha>
2025-06-13 15:27:12 +0100

    Add JSend helper class for standardized JSON responses

---
+++ modules/User/app/Helpers/JSend.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Helpers;
+
+class JSend
+{
+    public array $data = [];
+
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        $this->data = [];
+    }
+
+    public static function success(array $data)
+    {
+        return response()->json([
+            'status' => 'success',
+            'data' => $data,
+        ]);
+    }
+
+    public static function error(string $message, ?int $code = null, ?array $data = null)
+    {
+        $_data = [];
+        $_data['status'] = 'error';
+        $_data['message'] = $message;
+        $_data ? $_data['data'] = $data : null;
+
+        return response()->json($_data, $code);
+    }
+
+    public static function fail(array $data)
+    {
+        return response()->json([
+            'status' => 'fail',
+            'data' => $data,
+        ]);
+    }
+}
