9d8747f799015493afedc6fe8474f30d02637646
Solomon Ochepa <solomonochepa@gmail.com>
2025-06-18 05:07:38 +0100

    Update User module with model, requests, factories, migrations, and seeders

--- modules/User/database/factories/UserFactory.php
+++ modules/User/database/factories/UserFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Database\Factories;
+namespace Modules\User\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
@@ -24,7 +24,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'other_name' => fake()->name(),
+            'username' => fake()->unique()->userName(),
+            'phone' => fake()->unique()->phoneNumber(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
@@ -37,7 +41,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
---
+++ modules/User/database/seeders/UserSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Modules\User\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
+use Modules\User\App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $password = '$1Password;';
+        $users = [
+            [
+                'first_name' => 'Admin',
+                'last_name' => 'User',
+                'username' => 'admin',
+                'phone' => '+2340000000000',
+                'email' => 'admin@example.com',
+                'password' => $password,
+            ],
+            [
+                'first_name' => 'Demo',
+                'last_name' => 'User',
+                'username' => 'demo',
+                'phone' => '+2340000000001',
+                'email' => 'demo@example.com',
+                'password' => $password,
+            ],
+        ];
+
+        foreach ($users as $user) {
+            User::firstOrCreate(Arr::only($user, ['username', 'phone', 'email']), $user);
+        }
+    }
+}
