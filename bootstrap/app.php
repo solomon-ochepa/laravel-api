2715038c76dc52ce2c5870bbe8dbb960790f4519
Solomon Ochepa <solomonochepa@gmail.com>
2025-06-18 09:38:34 +0100

    Add middleware aliases for role and permission management

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->alias([
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+            // 'client' => CheckClientCredentials::class,
+        ]);
         $middleware->trustProxies('*');
         $middleware->validateCsrfTokens(except: [
             'oauth/token',
