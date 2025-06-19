3a541d1a3ecfa9a61d781805b6a8674896f180a6
elisha-mustapha <elisha-mustapha>
2025-06-19 10:08:39 +0100

    Add JSend helper for standardized JSON responses; update user API routes

--- modules/User/routes/api.php
+++ modules/User/routes/api.php
@@ -4,9 +4,5 @@ use Illuminate\Support\Facades\Route;
 use Modules\User\App\Http\Controllers\Api\UserController;
 
 Route::prefix('v1')->group(function () {
-    // Clients
-    Route::middleware(['client'])->group(function () {
-        Route::apiResource('users', UserController::class)->names('user');
-    });
-
+    Route::apiResource('users', UserController::class)->names('users');
 });
