6073902030cfb538c037ef7fee74578eeff388d3
elisha-mustapha <elisha-mustapha>
2025-07-02 08:19:20 +0100

    Add restore method and API route for restoring soft-deleted users

--- modules/User/routes/api.php
+++ modules/User/routes/api.php
@@ -5,7 +5,9 @@ use Modules\User\App\Http\Controllers\Api\UserController;
 
 Route::prefix('v1')->group(function () {
     Route::apiResource('users', UserController::class)->names('users');
+
     Route::prefix('users')->as('users.')->group(function () {
         Route::delete('/{user}/force', [UserController::class, 'force_delete'])->name('delete.force');
+        Route::patch('{user}/restore', [UserController::class, 'restore'])->name('restore');
     });
 });
