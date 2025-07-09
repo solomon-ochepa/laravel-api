1f862e150b28431f9658ceb3519486aba3af8a5a
Solomon Ochepa <solomonochepa@gmail.com>
2025-07-09 15:41:50 +0100

    Add RoleFactory

--- modules/Role/app/Models/Role.php
+++ modules/Role/app/Models/Role.php
@@ -3,15 +3,22 @@
 namespace Modules\Role\App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Modules\Role\Database\Factories\RoleFactory;
 use Spatie\Permission\Models\Role as SpatieRole;
 
 class Role extends SpatieRole
 {
-    use HasUuids, SoftDeletes;
+    use HasFactory, HasUuids, SoftDeletes;
 
     public function getRouteKeyName()
     {
         return 'name';
+    }
+
+    protected static function newFactory()
+    {
+        return RoleFactory::new();
     }
 }
