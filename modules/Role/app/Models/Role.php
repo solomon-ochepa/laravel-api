4f88019ecc2d8376b2ced09eade4607a4d646e4e
Solomon Ochepa <solomonochepa@gmail.com>
2025-07-09 15:45:11 +0100

    Add Searchable trait to Role model

--- modules/Role/app/Models/Role.php
+++ modules/Role/app/Models/Role.php
@@ -5,12 +5,13 @@ namespace Modules\Role\App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Scout\Searchable;
 use Modules\Role\Database\Factories\RoleFactory;
 use Spatie\Permission\Models\Role as SpatieRole;
 
 class Role extends SpatieRole
 {
-    use HasFactory, HasUuids, SoftDeletes;
+    use HasFactory, HasUuids, Searchable, SoftDeletes;
 
     public function getRouteKeyName()
     {
