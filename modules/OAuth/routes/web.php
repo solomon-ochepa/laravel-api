fcadedcc9a4c24fb56d2d247300a338405e8f1d1
Solomon Ochepa <solomonochepa@gmail.com>
2025-07-24 03:02:53 +0100

    Add the oauth() and callback() routes and methods

--- modules/OAuth/routes/web.php
+++ modules/OAuth/routes/web.php
@@ -1,3 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use Modules\OAuth\App\Http\Controllers\OAuthController;
+
+Route::get('oauth', [OAuthController::class, 'oauth'])->name('oauth');
+Route::get('auth/callback', [OAuthController::class, 'callback'])->name('oauth.callback');
+
 require_once 'passport.php';
